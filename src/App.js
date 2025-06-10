35a048baa5015b63a9066b2e75f158b1171a6fbe
Barrett Anderson <barrett1614@gmail.com>
2025-06-09 21:05:41 -0500

    Working on Login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Backend code
+    console.log("Logging in with", email, password);
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <h2>Login</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Log In</button>
+    </form>
+  );
+}
+
+export default Login;
