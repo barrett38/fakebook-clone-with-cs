f672dc20b3843ff02a8d524e69a8efb7bd49ef7a
Barrett Anderson <barrett1614@gmail.com>
2025-06-10 23:10:14 -0500

    Working on Profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div>
+      <h2>John Doe</h2>
+      <p>Email: johndoe@example.com</p>
+      <p>Posts: 10</p>
+    </div>
+  );
+}
+
+export default Profile;
