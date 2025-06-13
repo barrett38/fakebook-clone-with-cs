92de2bd440edcddedc003d6ef16c523afdbf805a
Barrett Anderson <barrett1614@gmail.com>
2025-06-13 13:34:25 -0500

    Working on Feed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
+import Feed from "./components/Feed/Feed";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Feed />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Feed/Feed.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Post from "../Posting/Post";
+import CreatePost from "../CreatePost/CreatePost";
+
+function Feed() {
+  const dummyPosts = [
+    { username: "John Doe", content: "Hello world!" },
+    { username: "Jane Smith", content: "React is awesome!" },
+  ];
+
+  return (
+    <div>
+      <CreatePost />
+      {dummyPosts.map((post, idx) => (
+        <Post key={idx} username={post.username} content={post.content} />
+      ))}
+    </div>
+  );
+}
+
+export default Feed;
